55b0e9ff93a2cd8c2c5b727e20311c59b24defec
Azarhel <azarhel.moriarty@gmail.com>
2024-04-05 13:03:49 -0600

    CROSS

--- api/index.js
+++ api/index.js
@@ -1,27 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+const express = require("express");
+const cors = require('cors'); // Importa el paquete CORS
+const app = express();
 
-function App() {
-  const [contacto, setContacto] = useState({});
+app.use(express.json()); // Middleware para parsear el cuerpo de las peticiones a JSON
+app.use(cors()); // Activa el middleware CORS en tu aplicación Express
 
-  useEffect(() => {
-    axios.get('http://localhost:3000/api/v1/contacto')
-      .then(response => {
-        setContacto(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+// Aquí van tus endpoints o rutas
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-  return (
-    <div>
-      <h1>Información de Contacto</h1>
-      <p>Nombre: {contacto.nombre}</p>
-      <p>Email: {contacto.email}</p>
-      <p>Mensaje: {contacto.mensaje}</p>
-    </div>
-  );
-}
+app.get("/api/v1/contacto", (req, res) => {
+    const respuesta = {
+        nombre: "Ejemplo Nombre",
+        email: "ejemplo@correo.com",
+        mensaje: "Este es un mensaje de ejemplo."
+    };
+    res.status(200).json(respuesta);
+});
 
-export default App;
+app.listen(3000, () => console.log("Server ready on port 3000."));
