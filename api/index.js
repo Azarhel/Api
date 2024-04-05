d19eaeb2ef3a70659c17f065552af264ee6b3bad
Azarhel <azarhel.moriarty@gmail.com>
2024-04-05 12:52:54 -0600

    axio

--- api/index.js
+++ api/index.js
@@ -1,18 +1,27 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-app.get("/", (req, res) => res.send("Express on Vercel"));
-// Nuevo endpoint /api/v1/contacto
-app.get("/api/v1/contacto", (req, res) => {
-    // Creando un objeto JSON para enviar como respuesta
-    const respuesta = {
-        nombre: "Ejemplo Nombre",
-        email: "ejemplo@correo.com",
-        mensaje: "Este es un mensaje de ejemplo."
-    };
-       // Enviando el objeto JSON como respuesta
-       res.status(200).json(respuesta);
-    });
-app.listen(3000, () => console.log("Server ready on port 3000."));
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-module.exports = app;
+function App() {
+  const [contacto, setContacto] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/v1/contacto')
+      .then(response => {
+        setContacto(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Información de Contacto</h1>
+      <p>Nombre: {contacto.nombre}</p>
+      <p>Email: {contacto.email}</p>
+      <p>Mensaje: {contacto.mensaje}</p>
+    </div>
+  );
+}
+
+export default App;
