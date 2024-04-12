7300c5b2f79e8071c18dfc0d1b3c775b1ecd593c
harsoz <66538679+harsoz@users.noreply.github.com>
2024-04-11 23:10:13 -0600

    fix cors

--- api/index.js
+++ api/index.js
@@ -3,17 +3,16 @@ const cors = require("cors"); // Importa el paquete CORS
 const app = express();
 const servicio = require("./service/fotoServicio");
 const corsOptions = {
-  origin: 'https://mi-primera-9yhp6wr85-azarhels-projects.vercel.app/',
+  origin: 'mi-primera-app-beta.vercel.app',
   optionsSuccessStatus: 200,
 };
 
 app.use(express.json()); // Middleware para parsear el cuerpo de las peticiones a JSON
-app.use(cors(corsOptions)); // Activa el middleware CORS en tu aplicación Express
 
 // Aquí van tus endpoints o rutas
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.post("/api/v1/contacto", async (req, res) => {
+app.post("/api/v1/contacto",cors(corsOptions), async (req, res) => {
   let respuesta = await servicio.registrarSolicitud(req.body);
   res.status(respuesta.status).json({ mensaje: respuesta.mensaje });
 });
