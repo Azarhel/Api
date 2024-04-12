264bbf5ebd0dd565c3d89a8ac6d915ec55894267
harsoz <66538679+harsoz@users.noreply.github.com>
2024-04-11 22:56:04 -0600

    configurando el cors para que ui pueda acceder al endpoint

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors"); // Importa el paquete CORS
 const app = express();
 const servicio = require("./service/fotoServicio");
+const corsOptions = {
+  origin: 'https://mi-primera-9yhp6wr85-azarhels-projects.vercel.app/',
+  optionsSuccessStatus: 200,
+};
 
 app.use(express.json()); // Middleware para parsear el cuerpo de las peticiones a JSON
 app.use(cors()); // Activa el middleware CORS en tu aplicación Express
@@ -9,7 +13,7 @@ app.use(cors()); // Activa el middleware CORS en tu aplicación Express
 // Aquí van tus endpoints o rutas
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.post("/api/v1/contacto", async (req, res) => {
+app.post("/api/v1/contacto", cors(corsOptions), async (req, res) => {
   let respuesta = await servicio.registrarSolicitud(req.body);
   res.status(respuesta.status).json({ mensaje: respuesta.mensaje });
 });
